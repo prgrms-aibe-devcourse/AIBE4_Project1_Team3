f1d5c08a7b54e0ad49f2be80f584b8129d5ee4ce
Glory0206 <dmb07223@gmail.com>
2025-10-13 11:11:00 +0900

    feat: express 서버 기본 틀 구성 및 추천 컨트롤러 작성 #7

---
+++ controllers/indexController.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const indexService = require("../services/indexService");
+
+const router = express.Router();
+
+router.post("/recommend", async (req, res) => {
+  const { startDate, endDate, budget, people } = req.body;
+
+  if (!startDate || !endDate || !budget || !people) {
+    return res.status(400).json({ error: "모든 필드를 입력해주세요." });
+  }
+
+  try {
+    const resultText = await indexService.recommend({
+      startDate,
+      endDate,
+      budget,
+      people,
+    });
+
+    res.send(resultText);
+  } catch (error) {
+    console.error("API 호출 erorr:", error);
+    res.status(500).json({ error: "API 호출 중 오류가 발생했습니다." });
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+
+module.exports = app;
+
+app.use(cors());
+app.use(express.json());
+
+const routes = require("./routes");
+app.use("/api", routes);
+
+app.listen(port, () => {
+  console.log(`서버가 ${port}번 포트로 실행 중입니다.`);
+});
