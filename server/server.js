ababac85d8f7524cd7c383a3c73ad76980fb0213
박지훈 <jihun6548@naver.com>
2025-10-16 17:16:44 +0900

    fix: 경로 수정

--- server/server.js
+++ server/server.js
@@ -4,28 +4,22 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import router from "./routes/index.js";
-
 dotenv.config();
-
 const app = express();
-const port = process.env.PORT || 10000;
-
+const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 app.use(cors());
 app.use(express.json());
-
-app.use(express.static(path.resolve("./src")));
+app.use(express.static(path.join(__dirname, "../src")));
 
 app.use("/api", router);
 
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.resolve("./src/index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../src/index.html"));
 });
 
 app.listen(port, () => {
   console.log(`서버가 ${port}번 포트로 실행 중입니다.`);
 });
-
 export default app;
